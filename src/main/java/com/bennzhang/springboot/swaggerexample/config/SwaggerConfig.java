5a7a5a887dcb64b0249cc569cea51f88cab485bc
Edgar Gonzalez <edgar.gonzalez@igac.gov.co>
2022-07-25 22:34:31 -0500

    archivos yml

--- src/main/java/com/bennzhang/springboot/swaggerexample/config/SwaggerConfig.java
+++ src/main/java/com/bennzhang/springboot/swaggerexample/config/SwaggerConfig.java
@@ -1,45 +1,35 @@
 package com.bennzhang.springboot.swaggerexample.config;
 
-
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket exampleApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bennzhang.springboot.swaggerexample"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metaInfo());
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerConfig.class, args);
     }
 
-    private ApiInfo metaInfo() {
 
-        ApiInfo apiInfo = new ApiInfo(
-                "Spring Boot Swagger Example API",
-                "Spring Boot Swagger Example Api Creator",
-                "1.0",
-                "Terms of Service",
-                new Contact("yourname", "https://github.com/yourlink",
-                        "youremail@yourdomain.com"),
-                "Apache License Version 2.0",
-                "https://www.apache.org/licesen.html"
-        );
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
 
-        return apiInfo;
+    @Bean
+    public Docket employeeApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
