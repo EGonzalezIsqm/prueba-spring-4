01f28c33bb729fb32a06c282d4f43a7af3864752
Edgar Gonzalez <edgar.gonzalez@igac.gov.co>
2022-07-25 22:18:44 -0500

    archivos yml

--- src/main/java/com/bennzhang/springboot/swaggerexample/SwaggerExampleApplication.java
+++ src/main/java/com/bennzhang/springboot/swaggerexample/SwaggerExampleApplication.java
@@ -1,17 +1,7 @@
-package com.hendisantika.swaggerexample;
+package com.bennzhang.springboot.swaggerexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * Created by IntelliJ IDEA.
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 9/3/17
- * Time: 7:58 AM
- * To change this template use File | Settings | File Templates.
- */
 
 @SpringBootApplication
 public class SwaggerExampleApplication {
